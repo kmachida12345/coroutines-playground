d7452ae4456e07391130cd6d189076f793c1c4c0
machi <kmachida12345@gmail.com>
2021-05-31 18:04:12 +0900

    ボタンをクリックした回数によって色を変える
    https://developer.android.com/codelabs/jetpack-compose-basics?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fcompose%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-basics#5

--- app/src/main/java/com/github/kmachida12345/coroutinesplayground/ui/component/Counter.kt
+++ app/src/main/java/com/github/kmachida12345/coroutinesplayground/ui/component/Counter.kt
@@ -1,12 +1,17 @@
 package com.github.kmachida12345.coroutinesplayground.ui.component
 
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(onClick = { updateCount(count + 1) }) {
+    Button(onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count % 2 == 0) Color.Green else Color.Red
+        )) {
         Text(text = "I've been clicked $count times!")
     }
 
